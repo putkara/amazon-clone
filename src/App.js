9029bba0730a9c6dd2b58a5628a7cf91d09f0961
Natia Meskhi <natia.meskhi.natia@gmail.com>
2021-03-11 20:16:55 +0100

    orders

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
+import Orders from "./Orders";
+import { useStateValue } from "./StateProvider";
+
+
 function App() {
+  const [{}, dispatch] = useStateValue();
   return (
-    //BEM
     <Router>
       <div className="app">
-        {/* Header */}
-        <Header />
         <Switch>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
+
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
-
-        {/* Home */}
       </div>
     </Router>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,81 +1,65 @@
 import React from "react";
 import "./Home.css";
-import banner from "./banner.jpg";
 import Product from "./Product";
-import alice from "./alice.jpg";
-import cattree from "./cattree.jpg";
-import samsung from "./samsung.jpg";
-import shose from "./shose.jpg";
-import screen from "./screen.png";
-import bag from "./bag.jpg";
-console.log(banner);
+
 function Home() {
   return (
     <div className="home">
-      <div className="home__conteiner">
-        <img className="home__image" src={banner} alt="banner" />
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          alt=""
+        />
+
         <div className="home__row">
           <Product
             id="12321341"
-            title="Alice in Wonderland,
-           Alice s Adventures in Wonderland 
-          (commonly shortened to Alice in Wonderland)
-           is an 1865 novel by
-           English author Lewis Carroll
-           "
-            price={25.5}
-            image={alice}
+            title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+            price={11.96}
             rating={5}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
           />
           <Product
-            id=" "
-            title="Buy Samsung Galaxy Note20 
-          and Note20 Ultra at Samsung PK.
-           Check out the price and limited 
-           time offers for your new Samsung
-            Galaxy mobile phone."
-            price={1200}
-            image={samsung}
-            rating={5}
-          />
-        </div>
-        <div className="home__row">
-          <Product
-            id=" "
-            title="The wooden parts are made of 18/30 mm
-           thick Multiplex."
-            price={460}
-            image={cattree}
-            rating={3}
-          />
-          <Product
-            id=" "
-            title="Customers who viewed KROSER Laptop
-           Bag 18 Inch Premium Business Briefcase
-           Fits up to 17.3 Inch… also viewed"
-            price={14}
-            image={bag}
-            rating={2}
-          />
-          <Product
-            id=" "
-            title="
-          Flipkart
-          Shoenstring Casual Shoes for Women sneakers
-           Girls canvas shoes latest stylish"
-            price={90}
-            image={shose}
-            rating={3}
-          />
-        </div>
-        <div className="home__row">
-          <Product
-            id=" "
-            title="Super-size your monitor for seamless
-           multi-tasking with the Samsung CJ89"
-            price={2000}
-            image={screen}
+            id="49538094"
+            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+            price={239.0}
             rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
+          />
+        </div>
+
+        <div className="home__row">
+          <Product
+            id="4903850"
+            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+            price={199.99}
+            rating={3}
+            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+          />
+          <Product
+            id="23445930"
+            title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+            price={98.99}
+            rating={5}
+            image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+          />
+          <Product
+            id="3254354345"
+            title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+            price={598.99}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+          />
+        </div>
+
+        <div className="home__row">
+          <Product
+            id="90829332"
+            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
+            price={1094.98}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
           />
         </div>
       </div>
